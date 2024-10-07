fbb5c58c94fe47933350461361bf6e893e674916
Sneha Soni <snehasoni@Snehas-MacBook-Air.local>
2024-10-07 23:57:48 +0530

    migration db

---
+++ migrations/20241007151603-create-ticket.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const { all } = require('../routes');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Tickets', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      subject: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      content: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      recepientEmail: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      status: {
+        type: Sequelize.ENUM,
+        values: ['Pending', 'SUCCESS', 'FAILED'],
+        defaultValue: 'Pending',
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Tickets');
+  }
+};
